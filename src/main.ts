ddfa940fd951268ee49150b4df285e4117b15a66
awpt1mus <kamehamehabeech@gmail.com>
2024-05-07 19:28:30 +0530

    hello world website

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import express,{ErrorRequestHandler, RequestHandler} from "express";
+import { join } from "path";
+
+const app = express();
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next)=> {
+    return res.sendFile(join(process.cwd(),"other","error.html"));
+}
+
+const notFoundHandler: RequestHandler = (req, res, next)=> {
+    return res.sendFile(join(process.cwd(),"other","404.html"));
+}
+
+app.use(express.static(join(process.cwd(),"public")));
+
+app.all("*",notFoundHandler);
+app.use(errorHandler);
+
+const port = 8080;
+
+app.listen(port,()=> {
+    console.log(`hello world listning on port ${port}`);
+});
